5b23d77afd15987ce585d565487a863651236c45
Jared De Blander <jared0x90@gmail.com>
2024-07-28 10:41:13 -0400

    laser cannons updates

---
+++ calibrating laser cannons v2.asm
@@ -0,0 +1,34 @@
+# load input to register 1
+in_to_reg1
+
+# copy reg1 to reg2
+reg1_to_reg2
+
+# add (reg3 = reg1 + reg2) 2x
+add
+
+# copy result back to reg1
+reg3_to_reg1
+
+# add (reg3 = reg1 + reg2) 3x
+add
+
+# copy result back to reg1
+reg3_to_reg1
+# add (reg3 = reg1 + reg2) 4x
+add
+
+# copy result back to reg1
+reg3_to_reg1
+
+# add (reg3 = reg1 + reg2) 5x
+add
+
+# copy result back to reg1
+reg3_to_reg1
+
+# add (reg3 = reg1 + reg2) 6x
+add
+
+# send out reseult
+reg3_to_out
