5679f79e2d34cec27a05f4760c4816d47046631c
Jared De Blander <jared0x90@gmail.com>
2024-07-28 10:54:01 -0400

    masking time

---
+++ second playthrough/masking time.asm
@@ -0,0 +1,14 @@
+# load input to register 1
+in_to_reg1
+
+# load register 0 
+3
+
+# copy reg0 to reg2
+reg0_to_reg2
+
+# reg3 = reg1 & reg2 (bitwise AND)
+and
+
+# write result to output
+reg3_to_out
